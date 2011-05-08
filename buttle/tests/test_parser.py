e82036837a8a832e07e1e28422fc236030bf9f9a
Rob Weir <rweir@ertius.org>
2011-05-08 14:39:22 +1000

    harder test

--- buttle/tests/test_parser.py
+++ buttle/tests/test_parser.py
@@ -19,6 +19,10 @@ class TokeniserTests(unittest.TestCase):
         line = """[["foo" "bar"]]"""
         self.assertEqual(tokenise(line), [['foo', 'bar']])
 
+    def test_tokenise_pairs_in_multis(self):
+        line = """[(["Mobile" "123"] ["Home" "456"])]"""
+        self.assertEqual(tokenise(line), [[['Mobile', '123'], ['Home', '456']]])
+
     def test_full_tokenise(self):
         line = """["Jane" "Doe" nil "Fake Pty Ltd" (["Mobile" "+61 4123 456 789"] ["Home" "61 2 9876 5432"]) nil ("someone@example.com") ((creation-date . "2001-01-01") (timestamp . "2002-02-02")) nil]"""
         tokens = ["Jane", "Doe", "nil", "Fake Pty Ltd", [["Mobile", "+61 4123 456 789"], ["Home", "61 2 9876 5432"]], 'nil', ["someone@example.com"], [['creation-date', "2001-01-01"], ['timestamp', "2002-02-02"]], 'nil']
