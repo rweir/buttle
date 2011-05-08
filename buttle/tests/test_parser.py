9736c02f23ce4fa7941a5a3c98ed9ca8836c4d32
Rob Weir <rweir@ertius.org>
2011-05-08 14:14:11 +1000

    rename to match scale

--- buttle/tests/test_parser.py
+++ buttle/tests/test_parser.py
@@ -3,7 +3,7 @@ import unittest
 from buttle.parser import tokenise
 
 class TokeniserTests(unittest.TestCase):
-    def test_tokenise(self):
+    def test_full_tokenise(self):
         line = """["Jane" "Doe" nil "Fake Pty Ltd" (["Mobile" "+61 4123 456 789"] ["Home" "61 2 9876 5432"]) nil ("someone@example.com") ((creation-date . "2001-01-01") (timestamp . "2002-02-02")) nil]"""
         tokens = ["Jane", "Doe", "nil", "Fake Pty Ltd", [["Mobile", "+61 4123 456 789"], ["Home", "61 2 9876 5432"]], 'nil', ["someone@example.com"], [['creation-date', "2001-01-01"], ['timestamp', "2002-02-02"]], 'nil']
         self.assertEqual(tokenise(line), tokens)
